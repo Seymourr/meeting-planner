20aa9ec90211fb9409089e890f6797e56016af84
david <davidlindstrom1989@gmail.com>
2016-04-20 16:32:26 +0200

    Added shared methods to activityCtrl

---
+++ app/js/controllers/activityCtrl.js
@@ -0,0 +1,44 @@
+meetingPlannerApp.controller('ActivityCtrl', function ($scope,$routeParams,Meeting, ngDialog) {
+  $scope.getDataWithPos = function(activity, index, dayID) {
+    activity.position = index;
+    if (dayID != undefined)
+      activity.oldDay = dayID;
+    else
+      activity.oldDay = null;
+    return activity;
+  };
+  $scope.onDropComplete = function (activity, $event, index, dayID) {
+      var day = activity.oldDay;
+      if(day === undefined) {
+        day = null;
+      }
+      if (dayID == undefined) {
+        dayID = null;
+      }
+      Meeting.moveActivity(day, activity.position, dayID, index);
+  };
+  $scope.openDialog = function (activity, index) {
+    activity.position = index;
+    activity.day = dayID;
+    ngDialog.open({
+        template: 'partials/createActivity.html',
+        className: 'ngdialog-theme-plain',
+        controller: 'CreateActivityCtrl',
+        data: activity
+    });
+  };
+  $scope.openDialog = function (activity, index, dayID) {
+    if(activity != null) {
+      if (dayID != undefined) {
+        activity.day = dayID;
+      }
+      activity.position = index;
+    }
+    ngDialog.open({
+        template: 'partials/createActivity.html',
+        className: 'ngdialog-theme-plain',
+        controller: 'CreateActivityCtrl',
+        data: activity
+        });
+  };
+});
